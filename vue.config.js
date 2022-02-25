6cac113d80725c47e9ea568e2b87ae2c6eda7553
skaarf9 <2791932480@qq.com>
2022-02-25 15:19:48 +0800

    add element-ui autoimport status

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,26 @@
+/*
+ * @Author: skaarf9
+ * @LastEditors: skaarf9
+ * @Date: 2022-02-25 01:00:20
+ * @LastEditTime: 2022-02-25 15:16:26
+ * @Description: file content
+ * @FilePath: \vueblog-vue\vue.config.js
+ */
+const AutoImport = require("unplugin-auto-import/webpack");
+const Components = require("unplugin-vue-components/webpack");
+const { ElementPlusResolver } = require("unplugin-vue-components/resolvers");
+
 const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
   transpileDependencies: true,
+  configureWebpack: {
+    plugins: [
+      AutoImport({
+        resolvers: [ElementPlusResolver()],
+      }),
+      Components({
+        resolvers: [ElementPlusResolver()],
+      }),
+    ],
+  },
 });
